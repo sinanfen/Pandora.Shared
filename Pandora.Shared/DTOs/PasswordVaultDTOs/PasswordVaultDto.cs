4250918d1d2e5fbe69c437f9a514eedf073e25cf
Sinan Fen <sinanfen@hotmail.com>
2024-10-08 21:51:19 +0300

    Add project files.

---
+++ Pandora.Shared/DTOs/PasswordVaultDTOs/PasswordVaultDto.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Pandora.Shared.DTOs.PasswordVaultDTOs;
+
+public class PasswordVaultDto : BaseDto<Guid>
+{
+    // User details
+    public Guid UserId { get; set; }
+
+    // Vault details
+    public string SecureSiteName { get; set; }  // e.g., "Facebook"
+    public string SecureUsernameOrEmail { get; set; }  // AES-encrypted username/email
+    public string SecureNotes { get; set; }  // AES-encrypted notes
+    public DateTime? LastPasswordChangeDate { get; set; }  // Last time password was changed
+    public DateTime? PasswordExpirationDate { get; set; }  // Optional expiration date for the password
+
+    // Category details
+    public Guid? CategoryId { get; set; }
+}
