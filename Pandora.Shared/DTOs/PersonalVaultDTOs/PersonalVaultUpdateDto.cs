57b2e7ee5e5cd2f61a2915cf15eb245fc5acf36c
Sinan Fen <sinanfen@hotmail.com>
2025-02-01 00:07:51 +0300

    package updated. (validations for PersonalVaults) locking mechanism created.

--- Pandora.Shared/DTOs/PersonalVaultDTOs/PersonalVaultUpdateDto.cs
+++ Pandora.Shared/DTOs/PersonalVaultDTOs/PersonalVaultUpdateDto.cs
@@ -1,11 +1,13 @@
 ﻿
+using Pandora.Shared.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace Pandora.Shared.DTOs.PersonalVaultDTOs;
 
 public class PersonalVaultUpdateDto : BaseDto<Guid>, IPersonalVaultDto
 {
     // User details
     public Guid UserId { get; set; }
-
     // Box content
     public string Title { get; set; }  // Box title (e.g., "Links to Watch Later")
     public string Content { get; set; }  // Unencrypted content (to be AES-encrypted)
@@ -13,10 +15,13 @@ public class PersonalVaultUpdateDto : BaseDto<Guid>, IPersonalVaultDto
     public string MediaFile { get; set; }  // Unencrypted media file path (to be AES-encrypted)
     public string Summary { get; set; }  // Optional summary of the content
     public IList<string> Tags { get; set; }  // Tags for organization
-
+    [Required]
+    public bool IsLocked { get; set; }
+    [RequiredIfTrue(nameof(IsLocked))]
+    [MinimumDateOffset(1, TimeUnit.Month)]
+    public DateTime? UnlockDate { get; set; }
     // Category
     public Guid? CategoryId { get; set; }  // Update category if needed
-
     // Metadata
     public DateTime? ExpirationDate { get; set; }  // Optional expiration date
     public bool IsFavorite { get; set; }  // Mark as favorite or not
